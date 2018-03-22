4fb6512bcffb3d5569ded1e96069820d28b39afe
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-03-23 00:36:22 +0430

    little changes

--- bin/pipenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-$DIR/exec.sh "ENV=dev pipenv $@"
-$DIR/exec.sh 'chown -R $uid:$uid /root/.local/share/virtualenvs && chown $uid:$uid Pipfile.lock'
